6a147c7a27d6ec40b8dc8329331bc50405a4caae
Leonardo Murakami <leonardo.murakami@loft.com.br>
2024-03-20 13:45:08 -0300

    [fix] fix some feedback related stuff

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [userCombination, setUserCombination] = useState([]);
-  const [results, setResults] = useState([]);
+  const [attempts, setAttempts] = useState([]);
+  const [currentAttempt, setCurrentAttempt] = useState([]);
 
   useEffect(() => {
     const app = document.querySelector('.App');
@@ -15,29 +15,33 @@ function App() {
 
   const handleKeyDown = (event) => {
     if (event.key.startsWith('Arrow')) {
-      setUserCombination([...userCombination, event.key.replace('Arrow', '').toLowerCase()]);
-    } else if (event.key === 'Enter' && userCombination.length > 0) {
+      setCurrentAttempt([...currentAttempt, event.key.replace('Arrow', '').toLowerCase()]);
+    } else if (event.key === 'Enter' && currentAttempt.length > 0) {
       checkCombination();
     }
   };
 
   const checkCombination = async () => {
     try {
-      // Use Axios to send the userCombination to the backend
       const response = await axios.post('http://localhost:8000/check', {
-        user_combination: userCombination,
+        user_combination: currentAttempt,
       });
-
-      // Set the results with the response from the backend
-      setResults(response.data.results);
+      setAttempts([...attempts, { id: new Date().getTime(), attempt: currentAttempt, results: response.data.results }]);
+      setCurrentAttempt([]); // Clear the current input for the next attempt
     } catch (error) {
       console.error('There was a problem with the Axios operation:', error);
-      // Log error.response.data if the error comes from the backend
       if (error.response) {
         console.error('Error response data:', error.response.data);
       }
     }
-    setUserCombination([]); // Reset for the next input
+  };
+
+  const calculateOpacity = (index, attemptsLength) => {
+    const maxOpacity = 1;
+    const minOpacity = 0.1;
+    const opacityStep = (maxOpacity - minOpacity) / attemptsLength;
+
+    return maxOpacity - opacityStep * index;
   };
 
 
@@ -53,13 +57,22 @@ function App() {
 
   return (
       <div className="App" tabIndex={0} onKeyDown={handleKeyDown}>
-        <div id="arrow-inputs">
-          {userCombination.map((arrow, index) => (
-              <span key={index} className={`arrow ${results[index] || ''}`}>
+        <div id="current-input">
+          {currentAttempt.map((arrow, index) => (
+              <span key={index} className="arrow">
             {getArrowIcon(arrow)}
           </span>
           ))}
         </div>
+        {attempts.slice(0).reverse().map((attemptObj, index) => (
+            <div key={attemptObj.id} className={`attempt ${attemptObj.id === attempts[attempts.length - 1].id ? 'animate-new-attempt' : ''}`} style={{opacity: calculateOpacity(index, attempts.length)}}>
+              {attemptObj.attempt.map((arrow, idx) => (
+                  <span key={idx} className={`arrow ${attemptObj.results[idx] || ''}`}>
+                    {getArrowIcon(arrow)}
+                  </span>
+              ))}
+            </div>
+        ))}
       </div>
   );
 }
